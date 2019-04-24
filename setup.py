611d081a086471cfd004acd65eac52c472554b34
Pavel Dedik <pavel.dedik@kiwi.com>
2019-04-24 14:26:12 +0200

    Add structlog-sentry

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+
+with open("requirements.in") as f:
+    install_requires = [line for line in f if line and line[0] not in "#-"]
+
+with open("test-requirements.in") as f:
+    tests_require = [line for line in f if line and line[0] not in "#-"]
+
+setup(
+    name="structlog-sentry",
+    version="1.0.0",
+    url="https://github.com/kiwicom/structlog-sentry",
+    author="Pavel Dedík",
+    author_email="pavel.dedik@kiwi.com",
+    packages=find_packages(),
+    install_requires=install_requires,
+    tests_require=tests_require,
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
